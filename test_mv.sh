ef6e71d9a6722ec68b3e8672110aef0f813b8283
elias-a <elias.alstead@gmail.com>
2020-06-28 16:11:09 -0400

    mv tests pt. 1

--- test_mv.sh
+++ test_mv.sh
@@ -2,31 +2,64 @@
 
 ### Test script for the `mv` command
 
+function prep {
+    if [ -d "test" ]; then rm -r "test"; fi
+    if [ -f "test.dat" ]; then rm "test.dat"; fi
+    if [ -f "test1.dat" ]; then rm "test1.dat"; fi
+    if [ -f "test2.dat" ]; then rm "test2.dat"; fi
+    if [ -d ".trash" ]; then /bin/rm -r .trash; fi
 
-# Prep test files
-if [ ! -d "test" ]; then
     mkdir "test"
-fi
-
-echo "test " > "test.dat"
-echo "test1" > "test1.dat"
-echo "test2" > "test2.dat"
-
+    echo "test" > "test.dat"
+    echo "test1" > "test1.dat"
+    echo "test2" > "test2.dat"
+}
 
 # 1) Move file to another file in the same directory.
 #    - mv file1 file2
 #    - If file2 exists, move file2 to .trash/file2
 function test1 {
 
-    return 0
+    pass=0
+
+    prep
+    mv "test.dat" "test1.dat"
+
+    # test.dat should no longer exist
+    if [ -f "test.dat" ]; then
+        echo "1"
+        pass=1
+    fi
+
+    # .trash directory should have been created 
+    if [ ! -d ".trash" ]; then 
+        echo "2"
+        pass=1
+    fi
+
+    # test1.dat should be in .trash directory
+    if [ ! -f ".trash/test1.dat"  ]; then 
+        echo "3"
+        pass=1
+    fi
+
+    # test1.dat should contain only the text from test.dat
+    text=$(<test1.dat)
+    if [ ! "$text" = "test" ]; then 
+        echo "4"
+        pass=1
+    fi
+
+    if (( $pass == 0 )); then
+        echo "PASS"
+    else
+        echo "FAIL"
+    fi
+
+    return $pass
 }
 
 test1
-if (( $? == 0 )); then
-    echo "PASS"
-else 
-    echo "FAIL"
-fi
 
 
 # 2) Move file to another file in a different directory.
@@ -37,14 +70,6 @@ function test2 {
     return 0
 }
 
-test2
-if (( $? == 0 )); then
-    echo "PASS"
-else
-    echo "FAIL"
-fi
-
-
 # 3) Move files to a directory.
 #    - mv file* dir/
 #    - If any of the files exist in dir/, move the 
@@ -53,12 +78,5 @@ function test3 {
 
     return 0
 }
-
-test3
-if (( $? == 0 )); then
-    echo "PASS"
-else
-    echo "FAIL"
-fi
 
 
