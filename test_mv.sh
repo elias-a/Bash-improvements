86d8bfcd4186bc371099a466f6803eabbbefe614
elias-a <elias.alstead@gmail.com>
2020-06-30 22:48:10 -0400

    mv tests pt. 4

--- test_mv.sh
+++ test_mv.sh
@@ -6,6 +6,7 @@ t="  "
 
 function prep {
     if [ -d "test" ]; then rm -r "test"; fi
+    if [ -d "test1" ]; then rm -r "test1"; fi
     if [ -f "test.dat" ]; then rm "test.dat"; fi
     if [ -f "test1.dat" ]; then rm "test1.dat"; fi
     if [ -f "test2.dat" ]; then rm "test2.dat"; fi
@@ -16,6 +17,7 @@ function prep {
     if [ -d "test/.trash" ]; then /bin/rm -r "test/.trash"; fi
 
     mkdir "test"
+    mkdir "test1"
     echo "test" > "test.dat"
     echo "test1" > "test1.dat"
     echo "test2" > "test2.dat"
@@ -108,5 +110,38 @@ function test3 {
 }
 
 test3
+
+
+# 4) Move a directory to another directory
+#    - mv dir1/ dir2/
+#    - if dir2/dir1/ exists, move dir2/dir1/ to
+#      dir2/.trash/dir1/
+function test4 {
+
+    echo "TEST 4"
+
+    prep
+    mkdir "test1/test/"
+    mv "test/" "test1/"
+
+    # test/ should no longer exist
+    if [ -d "test" ]; then echo "$t FAIL"; else echo "$t PASS"; fi 
+
+    # test1/test/ should exist
+    if [ ! -d "test1/test" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # test1/.trash should exist
+    if [ ! -d "test1/.trash" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # test1/.trash/test/ should exist
+    if [ ! -d "test1/.trash/test" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # test1/test/test*.dat should exist
+    if [ ! -f "test1/test/test.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+    if [ ! -f "test1/test/test1.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+    if [ ! -f "test1/test/test2.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+}
+
+test4
 
 
