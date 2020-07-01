01835b88c3f72d5aae2530cb2f024ecbde9fb004
elias-a <elias.alstead@gmail.com>
2020-06-30 22:07:48 -0400

    mv tests pt. 2

--- test_mv.sh
+++ test_mv.sh
@@ -2,17 +2,26 @@
 
 ### Test script for the `mv` command
 
+t="  "
+
 function prep {
     if [ -d "test" ]; then rm -r "test"; fi
     if [ -f "test.dat" ]; then rm "test.dat"; fi
     if [ -f "test1.dat" ]; then rm "test1.dat"; fi
     if [ -f "test2.dat" ]; then rm "test2.dat"; fi
-    if [ -d ".trash" ]; then /bin/rm -r .trash; fi
+    if [ -f "test/test.dat" ]; then rm "test/test.dat"; fi
+    if [ -f "test/test1.dat" ]; then rm "test/test1.dat"; fi
+    if [ -f "test/test2.dat" ]; then rm "test/test2.dat"; fi
+    if [ -d ".trash" ]; then /bin/rm -r ".trash"; fi
+    if [ -d "test/.trash" ]; then /bin/rm -r "test/.trash"; fi
 
     mkdir "test"
     echo "test" > "test.dat"
     echo "test1" > "test1.dat"
     echo "test2" > "test2.dat"
+    echo "test" > "test/test.dat"
+    echo "test1" > "test/test1.dat"
+    echo "test2" > "test/test2.dat"
 }
 
 # 1) Move file to another file in the same directory.
@@ -20,55 +29,53 @@ function prep {
 #    - If file2 exists, move file2 to .trash/file2
 function test1 {
 
-    pass=0
+    echo "TEST 1"
 
     prep
     mv "test.dat" "test1.dat"
 
     # test.dat should no longer exist
-    if [ -f "test.dat" ]; then
-        echo "1"
-        pass=1
-    fi
+    if [ -f "test.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
 
     # .trash directory should have been created 
-    if [ ! -d ".trash" ]; then 
-        echo "2"
-        pass=1
-    fi
+    if [ ! -d ".trash" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
 
     # test1.dat should be in .trash directory
-    if [ ! -f ".trash/test1.dat"  ]; then 
-        echo "3"
-        pass=1
-    fi
+    if [ ! -f ".trash/test1.dat"  ]; then echo "$t FAIL"; else echo "$t PASS"; fi
 
     # test1.dat should contain only the text from test.dat
-    text=$(<test1.dat)
-    if [ ! "$text" = "test" ]; then 
-        echo "4"
-        pass=1
-    fi
-
-    if (( $pass == 0 )); then
-        echo "PASS"
-    else
-        echo "FAIL"
-    fi
-
-    return $pass
+    text=$(<"test1.dat")
+    if [ ! "$text" = "test" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
 }
 
 test1
 
 
-# 2) Move file to another file in a different directory.
+# 2) Move file to another file with the same name in a different directory.
 #    - mv file1 dir/file1
 #    - If dir/file1 exists, move dir/file1 to dir/.trash/file1
 function test2 {
 
-    return 0
+    echo "TEST 2"
+
+    prep
+    mv "test.dat" "test/test.dat"
+
+    # test.dat should no longer exist
+    if [ -f "test.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # test/test.dat should exist
+    if [ ! -f "test/test.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # .trash directory should have been created
+    if [ ! -d "test/.trash" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
+
+    # test/test.dat should be in the test/.trash directory
+    if [ ! -f "test/.trash/test.dat" ]; then echo "$t FAIL"; else echo "$t PASS"; fi
 }
+
+test2
+
 
 # 3) Move files to a directory.
 #    - mv file* dir/
@@ -76,7 +83,11 @@ function test2 {
 #      duplicate file in dir/ to dir/.trash/
 function test3 {
 
-    return 0
+    prep
+
+
 }
+
+test3
 
 
